b0a914d78c9d9ea1b41050ca47a3cc548e3dfbbd
ThanhGiang <giangthihathanh@gmail.com>
2018-09-15 10:38:53 +0700

    Finish Topic 1 - Check environment

---
+++ src/selenium_api/Topic_01_CheckEnvironment.java
@@ -0,0 +1,41 @@
+package selenium_api;
+
+import org.testng.annotations.Test;
+
+
+import org.testng.annotations.BeforeClass;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.ImplicitlyWait;
+import org.testng.Assert;
+import org.testng.annotations.AfterClass;
+
+public class Topic_01_CheckEnvironment {
+	WebDriver driver;
+	
+	@BeforeClass
+  public void beforeClass() {
+	// Khởi tạo data/ pre-condition
+		driver = new FirefoxDriver();
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+	}
+
+@Test
+  public void TC_01_CheckBrowser() {
+	  // Function for Test Case
+	driver.get("http://live.guru99.com/");
+	String homePageTitle = driver.getTitle();
+	Assert.assertEquals(homePageTitle, "Home page");
+  }
+
+
+  @AfterClass
+  public void afterClass() {
+	  // Clear data
+	  driver.quit();
+  }
+
+}
