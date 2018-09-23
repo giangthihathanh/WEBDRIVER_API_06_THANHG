a0469f4fdcef893b7e52805752d673910a0c1a9f
thanh giang <giangthihathanh@gmail.com>
2018-09-23 13:11:52 +0700

    Topic 02

--- src/selenium_api/Topic_02_XPATH_CSS.java
+++ src/selenium_api/Topic_02_XPATH_CSS.java
@@ -25,11 +25,40 @@ public class Topic_02_XPATH_CSS {
 		//value="" name="login[username]"
 		
 		// Element = tagname[attribute='value']
-		driver.findElement(By.id("email")).sendKeys("Automation");
-		driver.findElement(By.className("f")).sendKeys("Automation");
-		driver.findElement(By.name("f")).sendKeys("Automation");
+		driver.findElement(By.id("email")).sendKeys("giangthihathanh@gmail.com");
+		driver.findElement(By.className("input-text required-entry validate-email")).sendKeys("giangthihathanh@gmail.com");
+		
+		driver.findElement(By.linkText("About Us")).click();
+		
+		// By CSS
+		driver.findElement(By.cssSelector("input[id='email']")).sendKeys("giangthihathanh@gmail.com\"");
+		
+		// Xpath
+		driver.findElement(By.xpath("//input[@id='email']")).sendKeys("giangthihathanh@gmail.com");
 		
 		
+		//Parent node
+		driver.findElement(By.xpath("//div[@class='footer']//a[@title='My Account']")).click();
+		
+		//Contain Text
+		driver.findElement(By.xpath("//input[contains(@id, 'ser-mess']")).sendKeys("Error 404");
+		
+		//Start with
+		driver.findElement(By.xpath("//div[@class='flipBannerWrap']//iframe[start-with(@id,'viz_iframe']")).click();
+		
+		//Or
+		driver.findElement(By.xpath("//@class='form-control' or @id='user-message']")).click();		
+		
+		//And
+		driver.findElement(By.xpath("//@id='user-message' and @class='form-control']")).click();
+		
+		//Following Sibling
+		driver.findElement(By.xpath("//td[text()='Customer ID']/following-sibling::td")).click();
+
+		//Preceding Sibling
+		driver.findElement(By.xpath("//span[text()='Total']/preceding-sibling::span")).click();
+	
+	
 	}
 	
 	@AfterClass
